9a1c1648f791901e01a7967b5087361847966c4d
bureson2@fel.cvut.cz <bureson2@fel.cvut.cz>
2021-12-27 17:03:40 +0100

    Contract

--- Semestral_work/Copyto/src/main/java/cz/cvut/kbss/ear/copyto/model/Contract.java
+++ Semestral_work/Copyto/src/main/java/cz/cvut/kbss/ear/copyto/model/Contract.java
@@ -1,8 +1,35 @@
 package cz.cvut.kbss.ear.copyto.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Contract extends AbstractEntity{
 
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private User client;
+
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private User copywriter;
+
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private Order order;
+
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private Date dateOfAgreement = new Date();
+
+    // TODO aby pri zmene deadlinu u objednavky zustalo zachovano puvodni domluvene datum
+    @Basic(optional = false)
+    @Column(nullable = true)
+    private Date date;
+
+    // TODO aby pri zmene castky u objednavky zustalo zachovano puvodni domluvena castka
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private double price;
+
+    @Basic(optional = false)
+    @Column(nullable = true)
+    private double penalty;
 }
