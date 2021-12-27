3b61e62612628cdd1962952dab40d6e3d934db23
bureson2@fel.cvut.cz <bureson2@fel.cvut.cz>
2021-12-27 16:15:10 +0100

    Prvni 2 entity pro dao

--- Semestral_work/Copyto/src/main/java/cz/cvut/kbss/ear/copyto/model/Category.java
+++ Semestral_work/Copyto/src/main/java/cz/cvut/kbss/ear/copyto/model/Category.java
@@ -1,4 +1,20 @@
 package cz.cvut.kbss.ear.copyto.model;
 
-public class Category {
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category extends AbstractEntity {
+
+    @ManyToMany(mappedBy = "categories")
+    private List<Order> orders = new ArrayList<>();
+
+    private String name;
+
+
+
 }
--- Semestral_work/Copyto/src/main/java/cz/cvut/kbss/ear/copyto/model/Order.java
+++ Semestral_work/Copyto/src/main/java/cz/cvut/kbss/ear/copyto/model/Order.java
@@ -1,4 +1,27 @@
 package cz.cvut.kbss.ear.copyto.model;
 
-public class Order {
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.OrderBy;
+
+import java.sql.Date;
+import java.util.List;
+
+@Entity
+public class Order extends AbstractEntity{
+
+    @ManyToMany
+    @OrderBy("name")
+    private List<Category> categories;
+
+    private double price;
+
+    private Date insertionDate;
+
+    private Date deadline;
+
+    private String link;
+
+
+
 }
