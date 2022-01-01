49d64b0ec63f0b874aed1bbb93daf3a3053f90c2
bureson2@fel.cvut.cz <bureson2@fel.cvut.cz>
2022-01-01 16:23:32 +0100

    Resty pro message controller

--- Semestral_work/Copyto/src/main/java/cz/cvut/kbss/ear/copyto/rest/MessageController.java
+++ Semestral_work/Copyto/src/main/java/cz/cvut/kbss/ear/copyto/rest/MessageController.java
@@ -5,6 +5,7 @@ import cz.cvut.kbss.ear.copyto.model.Conversation;
 import cz.cvut.kbss.ear.copyto.model.Message;
 import cz.cvut.kbss.ear.copyto.model.users.User;
 import cz.cvut.kbss.ear.copyto.rest.util.RestUtils;
+import cz.cvut.kbss.ear.copyto.security.model.AuthenticationToken;
 import cz.cvut.kbss.ear.copyto.service.MessageService;
 import cz.cvut.kbss.ear.copyto.service.UserService;
 import org.slf4j.Logger;
@@ -14,9 +15,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -47,11 +50,20 @@ public class MessageController {
     }
 
     @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER', 'ROLE_CLIENT', 'ROLE_COPYWRITER')")
-    @PostMapping(value = "author-id/{authorId}/ receiver-id/{receiverId}/text/{text}",consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> sendMessage(@PathVariable Integer authorId, @PathVariable Integer receiverId, @PathVariable String text) {
+    @PostMapping(value = "/receiver-id/{receiverId}/text/{text}",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> sendMessage(Principal principal, @PathVariable Integer receiverId, @PathVariable String text) {
+        final AuthenticationToken auth = (AuthenticationToken) principal;
+        final User author = userService.find(auth.getPrincipal().getUser().getId());
+        if(author == null){
+            throw NotFoundException.create("User", auth.getPrincipal().getUser().getId());
+        }
+        final User receiver = userService.find(receiverId);
+        if(receiver == null){
+            throw NotFoundException.create("User", auth.getPrincipal().getUser().getId());
+        }
         Message message = new Message();
-        message.setAuthor(userService.find(authorId));
-        message.setReceiver(userService.find(receiverId));
+        message.setAuthor(author);
+        message.setReceiver(receiver);
         message.setText(text);
         messageService.sendMessage(message);
         LOG.debug("Sended message {}.", message);
@@ -60,9 +72,13 @@ public class MessageController {
     }
 
     @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER', 'ROLE_CLIENT', 'ROLE_COPYWRITER')")
-    @PostMapping(value = "author-id/{authorId}/text/{text}/group-message",consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> sendGroupMessage(@RequestBody ArrayList<Integer> userIds, @PathVariable Integer authorId, @PathVariable String text) {
-        User author = userService.find(authorId);
+    @PostMapping(value = "/text/{text}/group-message",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> sendGroupMessage(Principal principal, @RequestBody ArrayList<Integer> userIds, @PathVariable Integer authorId, @PathVariable String text) {
+        final AuthenticationToken auth = (AuthenticationToken) principal;
+        User author = userService.find(auth.getPrincipal().getUser().getId());
+        if(author == null){
+            throw NotFoundException.create("User", auth.getPrincipal().getUser().getId());
+        }
         List<User> receivers = new ArrayList<>();
         for(Integer id : userIds){
             receivers.add(userService.find(id));
@@ -81,7 +97,7 @@ public class MessageController {
         return messageService.findMessages();
     }
 
-    // TODO opravneni useri
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping(value = "/id/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Message getById(@PathVariable Integer id) {
         final Message message = messageService.findMessage(id);
@@ -90,35 +106,59 @@ public class MessageController {
         } return message;
     }
 
-    // TODO opravneni useri
     @GetMapping(value = "/id-conversation/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Message> getByConversation(@PathVariable Integer id) {
+    public List<Message> getByConversation(Principal principal, @PathVariable Integer id) {
         final Conversation conversation = messageService.findConversation(id);
-        final List<Message> messages = messageService.findAllMessagesInConversation(conversation);
         if(conversation == null){
             throw NotFoundException.create("Conversation", id);
         }
-        return messages;
+        final List<Message> messages = messageService.findAllMessagesInConversation(conversation);
+        if(messages == null){
+            throw NotFoundException.create("Messages for conversation", id);
+        }
+        final AuthenticationToken auth = (AuthenticationToken) principal;
+        if(conversation.getUsers().contains(auth.getPrincipal().getUser())) {
+            // TODO otestovat jestli tenhle user je spravnej user nebo to musi byt pres id
+            return messages;
+        } else {
+            throw new AccessDeniedException("Cannot access conversation of someone else");
+        }
     }
 
-    // TODO opravneni ueri
     @GetMapping(value = "/id-author/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Message> getByAuthor(@PathVariable Integer id) {
+    public List<Message> getByAuthor(Principal principal, @PathVariable Integer id) {
         final User user = userService.find(id);
-        final List<Message> message = messageService.findMessagesByAuthor(user);
-        if (message == null) {
+        if(user == null){
             throw NotFoundException.create("Author", id);
-        } return message;
+        }
+        final List<Message> messages = messageService.findMessagesByAuthor(user);
+        if (messages == null) {
+            throw NotFoundException.create("Messages fo author", id);
+        }
+        final AuthenticationToken auth = (AuthenticationToken) principal;
+        if(auth.getPrincipal().getUser().equals(user.getId())) {
+            return messages;
+        } else {
+            throw new AccessDeniedException("Cannot access messages of someone else");
+        }
     }
 
-    // TODO opravneni useri
     @GetMapping(value = "/id-receiver/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Message> getByReceiver(@PathVariable Integer id) {
+    public List<Message> getByReceiver(Principal principal, @PathVariable Integer id) {
         final User user = userService.find(id);
-        final List<Message> message = messageService.findMessagesByReceiver(user);
-        if (message == null) {
+        if (user == null) {
             throw NotFoundException.create("Receiver", id);
-        } return message;
+        }
+        final List<Message> messages = messageService.findMessagesByReceiver(user);
+        if (messages == null) {
+            throw NotFoundException.create("Messages for receiver", id);
+        }
+        final AuthenticationToken auth = (AuthenticationToken) principal;
+        if(auth.getPrincipal().getUser().equals(user.getId())) {
+            return messages;
+        } else {
+            throw new AccessDeniedException("Cannot access messages of someone else");
+        }
     }
 
 }
